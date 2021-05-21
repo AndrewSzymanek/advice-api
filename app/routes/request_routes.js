5e5c7f832284a494566dcdd7432317c9b5f4b6ac
AndrewSzymanek <andrew.szymanek@comcast.net>
2021-05-21 08:23:17 -0500

    Confirm get request working with Postman

--- app/routes/request_routes.js
+++ app/routes/request_routes.js
@@ -1,17 +1,19 @@
 module.exports = function (app, db) {
   app.get("/getAdvice", (req, res) => {
-    db.collection("notes").find(() => {
-      if (err) {
-        res.send({ error: "An error has occurred" });
-      } else {
-        res.send(item);
-      }
-    });
+    db.collection("advice")
+      .find({})
+      .toArray(function (err, docs) {
+        if (err) {
+          res.send({ error: "An error has occurred" });
+        } else {
+          res.send(docs);
+        }
+      });
   });
 
   app.post("/submitAdvice", (req, res) => {
-    const note = { req };
-    db.collection("notes").insert(note, (err, result) => {
+    const note = { body: req.body.body };
+    db.collection("advice").insert(note, (err, result) => {
       if (err) {
         res.send({ error: "An error has occurred" });
       } else {
---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url:
+    "mongodb+srv://andrew:andrewDB123@notable.9ufvt.mongodb.net/goldnugs?retryWrites=true&w=majority",
+};
